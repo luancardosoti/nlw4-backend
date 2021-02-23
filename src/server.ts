f37becdcfc2d04fe56d24caec48923212143fc97
luancardosoti <luancardosoti@gmail.com>
2021-02-23 16:08:44 -0300

    day 2 finished

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World - NLW04" });
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/", (req, res) => {
-  // recebeu os dados para salvar
-  return res.json({ message: "Os dados foram salvos com sucesso!" });
-});
-
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3002, () => console.log("Server is running"));
