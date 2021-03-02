0858b9c63247f1fd8be84a40bb38f7691802c818
luancardosoti <luancardosoti@gmail.com>
2021-03-02 17:31:14 -0300

    day 3 finished

--- src/database/migrations/1614221058315-CreateSurveys.ts
+++ src/database/migrations/1614221058315-CreateSurveys.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsers1614085168578 implements MigrationInterface {
+export class CreateSurveys1614221058315 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'users',
+        name: 'surveys',
         columns: [
           {
             name: 'id',
@@ -12,11 +12,11 @@ export class CreateUsers1614085168578 implements MigrationInterface {
             isPrimary: true,
           },
           {
-            name: 'name',
+            name: 'title',
             type: 'varchar',
           },
           {
-            name: 'email',
+            name: 'description',
             type: 'varchar',
           },
           {
@@ -30,6 +30,6 @@ export class CreateUsers1614085168578 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('users');
+    await queryRunner.dropTable('surveys');
   }
 }
